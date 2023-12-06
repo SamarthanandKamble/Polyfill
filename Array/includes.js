8d427e298003cea8bab79207456679647938db3c
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-12-06 19:52:34 +0530

    Implemented custom includes with the second arg

---
+++ Array/includes.js
@@ -0,0 +1,25 @@
+Array.prototype.customIncludes = function (value, index = 0) {
+  let startIndex = 0;
+  if (index < 0) {
+    startIndex = this.length + index;
+    startIndex < 0 ? (startIndex = 0) : (startIndex = startIndex);
+  }
+  for (startIndex; startIndex < this.length; startIndex++) {
+    if (value === this[startIndex]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const array1 = [1, 2, 3];
+console.log(array1.customIncludes(3));
+// Expected output: true
+
+const pets = ["cat", "dog", "bat"];
+
+console.log(pets.customIncludes("cat", -3));
+// Expected output: true
+
+console.log(pets.customIncludes("at"));
+// Expected output: false
